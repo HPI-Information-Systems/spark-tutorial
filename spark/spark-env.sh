ea7f4b85a35c24dd3104f112144ba1212d6c41f9
CodeLionX <sebastian.schmidl@t-online.de>
2019-02-11 18:31:27 +0100

    add spark config dir

---
+++ spark/spark-env.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# This file is sourced when running various Spark programs.
+
+# spark binds to this IP
+#SPARK_LOCAL_IP="0.0.0.0"
+# public dns name of the driver program
+#SPARK_PUBLIC_DNS="localhost"
+
+SPARK_MASTER_OPTS="-Dspark.deploy.defaultCores=2"
+SPARK_WORKER_CORES=2
+SPARK_WORKER_MEMORY=4g
+#SPARK_WORKER_OPTS="-Dspark.worker.cleanup.enabled=true -Dspark.worker.cleanup.appDataTtl=3600"
