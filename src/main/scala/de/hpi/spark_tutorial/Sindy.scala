8871660b91efb89e87a3f97a81ccded274135ffd
leonbornemann <leonbornemann@gmx.de>
2020-06-22 14:53:34 +0200

    Initial commit of sbt-based version (much simpler than maven)

---
+++ src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -0,0 +1,11 @@
+package de.hpi.spark_tutorial
+
+import org.apache.spark.sql.SparkSession
+
+object Sindy {
+
+  def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
+
+    // TODO
+  }
+}
